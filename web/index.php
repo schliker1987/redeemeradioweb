88f19a8e77809f8fa2ba38b02c687af0787800d8
SuperHeroFM <founders@superherofm.com>
2021-11-16 21:58:33 -0600

    :)

--- web/index.php
+++ web/index.php
@@ -552,8 +552,8 @@
                 <div class="row">
                     <div class="col-md-8 col-md-offset-2">
 
-                        <h1 class="brand-heading"><span style='color:gold;'>Super</span><span style='color:;'>Hero</span><span style='color:gold;'>FM</span></h1>
-                        <p class="intro-text">
+                        <h1 class="brand-heading" style='text-shadow:2px 2px #000;'><span style='color:gold;'>Super</span><span style='color:;'>Hero</span><span style='color:gold;'>FM</span></h1>
+                        <p class="intro-text" style='text-shadow:2px 2px #000;'>
 
                             World's most amazing radio station featuring <a href="https://alexanderschliker.com/?ref=superherofm.com" class='page-scroll btn btn-default'>Alexander Schliker</a>, a man that flies as your host! Subject to <a href='#terms' class='page-scroll btn btn-default'>Terms</a> above the auspices of Golden Ticket, Ticket to Gold and Disabled to Gold.
 
